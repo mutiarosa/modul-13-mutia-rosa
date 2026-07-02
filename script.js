8baa02cc564bed2b55a657f6322d6b91bd86de84
Mutia Rosa <2402018@student.poltas.ac.id>
2026-07-02 11:24:30 +0700

    Memperbarui fungsi JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const deskripsi = document.getElementById("deskripsi");
+const btnUbah = document.getElementById("btnUbah");
+
+btnUbah.addEventListener("click", function () {
+    deskripsi.textContent =
+        "Latihan Git dan GitHub berhasil dijalankan.";
+});
